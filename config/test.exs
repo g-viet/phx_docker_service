a43b29220caef86cc74e21ad896756c2ca6e3413
Viet <g-viet@eole.co.jp>
2018-07-11 16:56:00 +0900

    Add TravisCI config
    Update TravisCI config
    
    Update TravisCI config
    
    Update TravisCI config
    
    Update TravisCI config
    
    Update test config
    
    Update test config
    
    Update test config
    
    Update TravisCI config
    
    Add priv/repo/migrations/ directory
    
    Update TravisCI config
    
    Update TravisCI config
    
    Debug TravisCI config
    
    Debug TravisCI config
    
    Debug TravisCI config
    
    Debug TravisCI config

--- config/test.exs
+++ config/test.exs
@@ -13,7 +13,7 @@ config :logger, level: :warn
 config :phx_docker_service, PhxDockerService.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
+  password: "",
   database: "phx_docker_service_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
