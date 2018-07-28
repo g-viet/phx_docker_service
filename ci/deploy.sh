d4b04ca48e9d4c5bcd623f3426cb12dd73817936
g-viet <vietbktn@gmail.com>
2018-07-28 15:13:23 +0900

    TEST CI for elixir/phoenix project

---
+++ ci/deploy.sh
@@ -0,0 +1,20 @@
+set -e
+
+base_dir=$(dirname $0)
+
+if [ ! -f .version ]; then
+  echo "No version change, skip deployment"
+else
+  VERSION=$(cat .version)
+  echo "Replace Version"
+  ${base_dir}/steps/replace_version.sh ${VERSION}
+
+  echo "Build Release"
+  ${base_dir}/steps/build_release.sh
+
+  echo "Build Run Container"
+  ${base_dir}/steps/build_container.sh ${VERSION}
+
+  echo "Push to Docker Hub"
+  ${base_dir}/steps/push_to_docker_hub.sh ${VERSION} ${DOCKER_USER} ${DOCKER_PASS}
+fi
---
+++ ci/steps/build_container.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker build --build-arg VERSION=${1} -t leifg/time_tracking .
