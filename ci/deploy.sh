368cba517e8982c0f7fe9e692d65d51dd0313721
g-viet <vietbktn@gmail.com>
2018-07-28 16:34:57 +0900

    Test CI

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -2,9 +2,9 @@ set -e
 
 base_dir=$(dirname $0)
 
-if [ ! -f .version ]; then
-  echo "No version change, skip deployment"
-else
+# if [ ! -f .version ]; then
+#   echo "No version change, skip deployment"
+# else
   VERSION=$(cat .version)
   echo "Replace Version"
   ${base_dir}/steps/replace_version.sh ${VERSION}
@@ -12,9 +12,9 @@ else
   echo "Build Release"
   ${base_dir}/steps/build_release.sh
 
-  echo "Build Run Container"
-  ${base_dir}/steps/build_container.sh ${VERSION}
+  # echo "Build Run Container"
+  # ${base_dir}/steps/build_container.sh ${VERSION}
 
-  echo "Push to Docker Hub"
-  ${base_dir}/steps/push_to_docker_hub.sh ${VERSION} ${DOCKER_USER} ${DOCKER_PASS}
-fi
+  # echo "Push to Docker Hub"
+  # ${base_dir}/steps/push_to_docker_hub.sh ${VERSION} ${DOCKER_USER} ${DOCKER_PASS}
+# fi
