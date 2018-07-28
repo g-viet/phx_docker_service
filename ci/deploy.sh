7c1a5d414a3b386cd92653941b9367a6409d4b5d
g-viet <vietbktn@gmail.com>
2018-07-28 16:36:38 +0900

    Test CI

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -5,12 +5,15 @@ base_dir=$(dirname $0)
 # if [ ! -f .version ]; then
 #   echo "No version change, skip deployment"
 # else
-  VERSION=$(cat .version)
-  echo "Replace Version"
-  ${base_dir}/steps/replace_version.sh ${VERSION}
+  # VERSION=$(cat .version)
+  # echo "Replace Version"
+  # ${base_dir}/steps/replace_version.sh ${VERSION}
 
   echo "Build Release"
   ${base_dir}/steps/build_release.sh
+
+  echo "Run Service"
+  ${base_dir}/steps/run_service.sh
 
   # echo "Build Run Container"
   # ${base_dir}/steps/build_container.sh ${VERSION}
